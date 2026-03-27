c4e833672b6ba8e587c9f2b9ca7ff6d7446514bd
Joel Gray <joel@smartz.com>
2026-03-27 14:13:47 -0700

    style: removing commented code

--- src/LoopMeet.Api/Services/Configuration/SupabaseConfigOptions.cs
+++ src/LoopMeet.Api/Services/Configuration/SupabaseConfigOptions.cs
@@ -20,40 +20,4 @@ public class SupabaseConfigOptions
                 options.JwtAudience = configuration["Supabase:JwtAudience"] ?? "authenticated";
                 options.AvatarBucketName = configuration["Supabase:AvatarBucketName"] ?? "avatars";
         }
-        //         options.Url = ConfigurationResolver.ResolveConfigValue(
-        //                 configuration,
-        //                 string.Empty,
-        //                 "Supabase:Url",
-        //                 "SUPABASE__URL",
-        //                 "SUPABASE_URL");
-        //         options.AnonOrPublishableKey = ConfigurationResolver.ResolveConfigValue(
-        //                 configuration,
-        //                 string.Empty,
-        //                 "Supabase:AnonOrPublishableKey",
-        //                 "SUPABASE__ANON_OR_PUBLISHABLE_KEY",
-        //                 "SUPABASE_ANON_OR_PUBLISHABLE_KEY",
-        //                 "Supabase:AnonKey",
-        //                 "SUPABASE__ANONKEY",
-        //                 "SUPABASE_ANONKEY",
-        //                 "SUPABASE_ANON_KEY");
-        //         options.ServiceOrSecretKey = ConfigurationResolver.ResolveConfigValue(
-        //                 configuration,
-        //                 string.Empty,
-        //                 "Supabase:ServiceOrSecretKey",
-        //                 "SUPABASE__SERVICE_OR_SECRET_KEY",
-        //                 "SUPABASE_SERVICE_OR_SECRET_KEY");
-        //         options.JwtIssuer = ConfigurationResolver.ResolveConfigValue(
-        //                 configuration,
-        //                 string.Empty,
-        //                 "Supabase:JwtIssuer",
-        //                 "SUPABASE__JWT_ISSUER",
-        //                 "SUPABASE_JWT_ISSUER");
-        //         options.AvatarBucketName = ConfigurationResolver.ResolveConfigValue(
-        //                 configuration,
-        //                 defaultValue: "avatars",
-        //                 "Supabase:AvatarBucketName",
-        //                 "SUPABASE__AVATAR_BUCKET_NAME",
-        //                 "SUPABASE_AVATAR_BUCKET_NAME"
-        //                 );
-        // }
 }
